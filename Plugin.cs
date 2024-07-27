755f2d2e26fadf320886e577a1caedaeee0277d3
aoura <76926458+aouradev@users.noreply.github.com>
2024-07-26 22:29:11 -0600

    Initial Release

---
+++ Plugin.cs
@@ -0,0 +1,58 @@
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using UnityEngine;
+
+namespace EmotionalSupport
+{
+
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+
+        public static AssetBundle bundle;
+        public static GameObject car;
+
+        void Start()
+        {
+            Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("EmotionalSupport.Resources.cmcat");
+            bundle = AssetBundle.LoadFromStream(stream);
+            if (stream == null)
+            {
+                Debug.LogError("STREAM IS NULL. NOT LOADING ASSETS");
+            }
+            stream.Close();
+
+            car = Instantiate(bundle.LoadAsset<GameObject>("catP"));
+
+            Mesh carM = bundle.LoadAsset<Mesh>("catmesh");
+            Material catMat = bundle.LoadAsset<Material>("catM");
+
+            Renderer carR = car.GetComponent<Renderer>();
+            carR.material = catMat;
+
+            MeshFilter carMeshFilter = car.GetComponent<MeshFilter>();
+
+            carMeshFilter.sharedMesh = carM;
+
+            Texture carT = bundle.LoadAsset<Texture>("catT");
+
+            carR.material = catMat;
+            catMat.mainTexture = carT;
+
+            car.transform.position = new Vector3(-68.853f, 11.472f, -83.271f);
+            car.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            car.transform.localRotation = Quaternion.Euler(new Vector3(-95, 90, 0));
+
+            car.AddComponent<MeshCollider>();
+            car.AddComponent<GorillaSurfaceOverride>();
+            
+            GorillaSurfaceOverride carGorillaSurfaceOverride = car.GetComponent<GorillaSurfaceOverride>();
+
+            carGorillaSurfaceOverride.overrideIndex = 3;
+
+            Debug.Log("car");
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace EmotionalSupport
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.author.gorillatag.emotionalsupport";
+        public const string Name = "EmotionalSupport";
+        public const string Version = "1.0.0";
+    }
+}
